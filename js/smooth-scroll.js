678e4bfa0c3a0b1b028232bc24c03975986658fc
Karl Jasen Ricardo Tapangan <43909035+karljasen28@users.noreply.github.com>
2021-12-03 15:10:45 +0800

    Add files via upload
    Added Folder: Web Portfolio version 3.0

---
+++ js/smooth-scroll.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    // smooth scrolling 
+    $('a[href*="#"]').on('click',function(e){
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
+      },
+        1000, 
+        'linear'
+      );
+    });
+});
